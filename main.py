7c2d3192dd030d9304130f3f1933ce4bc8291613
Dolise <dmitrytagincev@redcollar.ru>
2022-08-17 17:21:07 +0300

    fixes

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 import telebot
 from telebot import types
 import buttons
+from flask import Flask, request
 
 allowed_users = json.loads(os.getenv("ALLOWED_IDS"))
 token = os.getenv("TOKEN")
+server = Flask(__name__)
 
 
 bot = telebot.TeleBot(token)
@@ -90,20 +92,21 @@ def answer(message):
             "https://forms.gle/UL6hVR1nALW9TLgS6"
         )
 
-if os.getenv("HEROKU"):
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://balanceburo.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+
+@server.route('/' + token, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
     bot.remove_webhook()
-    bot.polling(none_stop=True)
+    bot.set_webhook(url='https://balanceburo.herokuapp.com' + token)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
