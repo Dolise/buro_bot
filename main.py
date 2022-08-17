8ebb1af7756eaaf99ceeb6a2c7a7a028a0e66bd5
Dolise <dmitrytagincev@redcollar.ru>
2022-08-17 18:05:23 +0300

    fixes

--- main.py
+++ main.py
@@ -112,4 +112,4 @@ def webhook():
 if __name__ == "__main__":
     print("STARTED")
     logging.basicConfig(level=logging.INFO)
-    server.run(host="0.0.0.0", port=5000)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
