ba188a6a1776e2c028aef1db961fd3453b004b64
Dolise <dmitrytagincev@redcollar.ru>
2022-08-17 17:15:44 +0300

    webhook

--- main.py
+++ main.py
@@ -90,5 +90,20 @@ def answer(message):
             "https://forms.gle/UL6hVR1nALW9TLgS6"
         )
 
-
-bot.infinity_polling()
+if os.getenv("HEROKU"):
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://balanceburo.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
